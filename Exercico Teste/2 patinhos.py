711b4db29938e7683fa0dc177e78ccbf124d9f77
AndrezaGomesC <andrezagomesc18@gmail.com>
2025-03-23 16:14:10 -0300

    Initial commit

---
+++ Exercico Teste/2 patinhos.py
@@ -0,0 +1,23 @@
+"""nun = "2"
+texto =(" patinhos ")
+print (nun + texto)"""
+
+"""nun1 = input (" Digite um numero ")
+nun1_int = int (nun1)
+nun2 = input (" Digite outro numero ")
+nun2_int = int(nun2)
+
+soma = nun1_int + nun2_int 
+print ("A soma é: ", soma)"""
+
+"""nun1 = int( input (" Digite um numero "))
+nun2 =  int (input (" Digite outro numero"))
+
+print (" nun1 e maior que nun2? ",  nun1>nun2)
+print ("nun1 e menor que nun2? ", nun1<nun2)
+print (" nun1 é igual a nun2? ", nun1== nun2)"""
+
+(12>10)and(10>12)
+
+
+
